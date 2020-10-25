b4ba659c8ce7852c25bc72cf7d797f7a30341311
crismcarrera <crismcarrera@gmail.com>
2020-10-25 20:03:23 -0300

    primer commit de mi sitio web

---
+++ main.js
@@ -0,0 +1,12 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.showcase2');
+ScrollReveal().reveal('.showcase3');
+ScrollReveal().reveal('.showcase4');
+ScrollReveal().reveal('.showcase5');
+ScrollReveal().reveal('.showcase6');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
